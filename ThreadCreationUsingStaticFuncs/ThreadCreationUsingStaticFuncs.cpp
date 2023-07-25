d44cf5ff5d9c56d2a6915042d9fa73564be4ef32
Abel <belaashu23@gmail.com>
2023-07-25 13:02:59 +0200

    Modified the cout statements

--- ThreadCreationUsingStaticFuncs/ThreadCreationUsingStaticFuncs.cpp
+++ ThreadCreationUsingStaticFuncs/ThreadCreationUsingStaticFuncs.cpp
@@ -5,7 +5,7 @@ class Abel {
 public: 
 	static void countDown(int count) {
 		for (int i = 0; i < count; i++) {
-			std::cout <<"\nHellow Bel, things are getting easier" << std::endl; 
+			std::cout <<"\nHello Belaye, things are getting easier" << std::endl;
 			std::cout << "Thank God for that!" << std::endl; 
 		}
 	}
@@ -14,6 +14,8 @@ public:
 int main() {
 	std::thread Thread1(&Abel::countDown, 3); // Crating a thread using a static 
 	Thread1.join();							 // member function. 
+
+	std::cout << "\nRembember Bela underdogs seem slow at the begining. Remember that!!" << std::endl; 
 
 	return 0; 
 }
